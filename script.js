2a9ae6cd5357ee6c40daf28c63aeddfc4b75c460
Ishrathashim-stack <ishrathashim5@gmail.com>
2026-01-19 20:17:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scroll CTA
+// document.getElementById("ctaBtn").addEventListener("click", () => {
+   //  document.getElementById("contact").scrollIntoView({
+       //  behavior: "smooth"
+   //  });
+// });
+
+// WhatsApp Contact button
+// function openWhatsApp() {
+   //  window.open("https://wa.me/92XXXXXXXXXX", "_blank"); // Replace with your number
+// }
+// Smooth scroll when "Book a Session" clicked
+document.getElementById("ctaBtn").addEventListener("click", () => {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
+
+// WhatsApp contact button
+function openWhatsApp() {
+    window.open("https://wa.me/03118403594", "_blank"); // Replace with your number
+}
+
